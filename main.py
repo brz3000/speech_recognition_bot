2dce147d16b52cbdf7d23b17c2246f3bdf48cac4
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-09-18 18:04:05 +0300

    Update main.py
    add no action else  "intent.is_fallback"

--- main.py
+++ main.py
@@ -3,24 +3,45 @@ import random
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 from dotenv import load_dotenv
+from google.cloud import dialogflow_v2beta1 as dialogflow
 
 
-def echo(event, vk_api):
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1,1000)
+def detect_intent_texts(project_id, session_id, texts, language_code = "ru"):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=texts, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
     )
+    if response.query_result.intent.is_fallback:
+        return False
+    else:
+        return response.query_result.fulfillment_text
+
+
+
+def message_reply(event, vk_api, project_id):
+    is_fallback = detect_intent_texts(project_id, event.user_id, event.text, language_code = "ru")
+    if is_fallback:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=is_fallback,
+            random_id=random.randint(1,1000)
+        )
+    else:
+        pass
+
 
 def main():
     load_dotenv()
+    project_id = os.environ['GOOGLE_PROJECT_ID']
     vk_session = vk.VkApi(token=os.environ['VK_GROUP_ACCESS_TOKEN'])
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
-    print (longpoll.listen())
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            message_reply(event, vk_api, project_id)
 
 
 if __name__ == '__main__':
