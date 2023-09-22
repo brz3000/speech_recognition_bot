c26c354680d9b0bd15d2463b296f43f2c18dbff2
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-09-23 01:40:14 +0300

    Update main.py
    add bot_logger

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import os
 import random
+import telegram
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 from dotenv import load_dotenv
 from google.cloud import dialogflow_v2beta1 as dialogflow
 from time import sleep
+import logging
+
+
+logger = logging.getLogger('new_logger')
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
 
 
 def detect_intent_texts(project_id, session_id, texts, language_code="ru"):
@@ -38,6 +56,11 @@ def message_reply(event, vk_api, project_id):
 
 def main():
     load_dotenv()
+    chat_id = os.environ['TLG_CHAT_ID']
+    bot_logger = telegram.Bot(token=os.environ['TLG_TOKEN_LOGGER_BOT'])
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(TelegramLogsHandler(bot_logger, chat_id))
+    logger.info('Бот оповещений запущен')
     project_id = os.environ['GOOGLE_PROJECT_ID']
     vk_session = vk.VkApi(token=os.environ['VK_GROUP_ACCESS_TOKEN'])
     vk_api = vk_session.get_api()
@@ -47,9 +70,14 @@ def main():
             for event in longpoll.listen():
                 if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                     message_reply(event, vk_api, project_id)
-        except Exception as ex:
-            print(ex)
-            sleep(30)
+        except requests.exceptions.ConnectionError:
+            sleep(5)
+        except requests.exceptions.ReadTimeout:
+            pass
+        except Exception as err:
+            logger.error("Бот оповещений упал с ошибкой:")
+            logger.error(err, exc_info=True)
+            sleep(50)
 
 
 if __name__ == '__main__':
