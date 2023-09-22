71aec666772cb75a491b8eb86c9b09f60aa96c5f
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-09-22 18:00:02 +0300

    Update main.py
    Fix to PEP8

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from google.cloud import dialogflow_v2beta1 as dialogflow
 from time import sleep
 
 
-def detect_intent_texts(project_id, session_id, texts, language_code = "ru"):
+def detect_intent_texts(project_id, session_id, texts, language_code="ru"):
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(project_id, session_id)
     text_input = dialogflow.TextInput(text=texts, language_code=language_code)
@@ -21,14 +21,16 @@ def detect_intent_texts(project_id, session_id, texts, language_code = "ru"):
         return response.query_result.fulfillment_text
 
 
-
 def message_reply(event, vk_api, project_id):
-    is_fallback = detect_intent_texts(project_id, event.user_id, event.text, language_code = "ru")
+    is_fallback = detect_intent_texts(project_id,
+                                      event.user_id,
+                                      event.text,
+                                      language_code="ru")
     if is_fallback:
         vk_api.messages.send(
             user_id=event.user_id,
             message=is_fallback,
-            random_id=random.randint(1,1000)
+            random_id=random.randint(1, 1000)
         )
     else:
         pass
