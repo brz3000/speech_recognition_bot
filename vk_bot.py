15087b32fc8ea3e606d5c457a5a6963f5ccf306b
brz3000 <jons_borzov@mail.ru>
2023-09-25 22:11:53 +0300

    Update vk_bot.py
    Fix variable, moved the function to a separate module

--- vk_bot.py
+++ vk_bot.py
@@ -4,9 +4,9 @@ import telegram
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 from dotenv import load_dotenv
-from google.cloud import dialogflow_v2beta1 as dialogflow
 from time import sleep
 import logging
+from detect_itent import detect_intent_texts
 
 
 logger = logging.getLogger('new_logger')
@@ -24,34 +24,17 @@ class TelegramLogsHandler(logging.Handler):
         self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
-
-def detect_intent_texts(project_id, session_id, texts, language_code="ru"):
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-    text_input = dialogflow.TextInput(text=texts, language_code=language_code)
-    query_input = dialogflow.QueryInput(text=text_input)
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-    if response.query_result.intent.is_fallback:
-        return False
-    else:
-        return response.query_result.fulfillment_text
-
-
 def message_reply(event, vk_api, project_id):
-    is_fallback = detect_intent_texts(project_id,
+    itent_found = detect_intent_texts(project_id,
                                       event.user_id,
                                       event.text,
                                       language_code="ru")
-    if is_fallback:
+    if itent_found:
         vk_api.messages.send(
             user_id=event.user_id,
-            message=is_fallback,
+            message=itent_found,
             random_id=random.randint(1, 1000)
         )
-    else:
-        pass
 
 
 def main():
@@ -70,10 +53,6 @@ def main():
             for event in longpoll.listen():
                 if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                     message_reply(event, vk_api, project_id)
-        except requests.exceptions.ConnectionError:
-            sleep(5)
-        except requests.exceptions.ReadTimeout:
-            pass
         except Exception as err:
             logger.error("Бот оповещений упал с ошибкой:")
             logger.error(err, exc_info=True)
